f4116f50496a396179ced6ed7a91025adc49d593
vanessa-b-campbell <colorfulwaves@gmail.com>
2023-07-11 13:59:17 -0400

    functional group search debugging

--- get_functional_groups.py
+++ get_functional_groups.py
@@ -10,7 +10,7 @@ import re
 SMILEs_data = pd.read_csv("/home/jbd3qn/Downloads/critical-Temp-LNN/clean_smile_dataset.csv")
 
 
-
+# ('[Sn]', silicon_list),
 smiles_list = SMILEs_data['SMILEs'].tolist()
 
 # benzene, phenol group, carboxylic acid, amine
@@ -20,8 +20,11 @@ carboxylic_list = []
 fluorine_list = []
 nitrogen_list = []
 silicon_list = []
-func_group_list = [('[Si]', silicon_list),('C1=CC=C(C=C1)O', phenol_list), ('C1=CC=CC=C1', benzene_list), 
-                   (r"OC([*])=O", carboxylic_list), ('F', fluorine_list), ('N', nitrogen_list)]
+xe_list = []
+func_group_list = [('C1=CC=C(C=C1)O', phenol_list, 'phenol'), ('C1=CC=CC=C1', benzene_list, 'benzene'), 
+                   ('C(=O)O', carboxylic_list, 'carboxylic acid'), ('F', fluorine_list, 'fluorine'), 
+                   ('N', nitrogen_list, 'nirogen'), ('[Si]', silicon_list, 'silicon'), 
+                   ('[Xe]', xe_list, 'Xe')]
 
 gutter_list = []
 
@@ -36,22 +39,35 @@ for func_i in range(0, len(func_group_list)):
     for smile in smiles_list:
 
         mol = Chem.MolFromSmiles(smile)
-
-        if mol.HasSubstructMatch(Chem.MolFromSmiles(func_group_list[func_i][0])): # will return a true or false
+        match = mol.HasSubstructMatch(Chem.MolFromSmiles(func_group_list[func_i][0]))
+        print(match)
+        if match: # will return a true or false
             (func_group_list[func_i][1]).append(smile)
         
         
-        # if re.search(func_group_list[func_i][0], smile):        
-        #     (func_group_list[func_i][1]).append(smile)
+        # if re.findall(func_group_list[func_i][0], smile):        
+        #      (func_group_list[func_i][1]).append(smile)
 
-        else: 
-            gutter_list.append(smile)
+        if match == False: 
+            if smile not in gutter_list:
+                gutter_list.append(smile)
+    
+    # Statistics
+    percent = (len(func_group_list[func_i][1])/len(smiles_list))*100
+    print('\n')
+    print("{:.2f}%'of molecules have a {} group".format(percent,func_group_list[func_i][2]))
+    
+
+leftover = (len(gutter_list)/len(smiles_list))*100
+print("{:.2f}%'are left".format(leftover))
 
 
-print("phenol: {}".format(phenol_list))
-print('\n')
-print("benzene: {}".format(benzene_list))
-print('\n')
-print("carboxylic acid group: {}".format(carboxylic_list))
-print("gutter list: {}".format(gutter_list))
-print("silicon: {}".format(silicon_list))
+
+# print("phenol: {}".format(phenol_list))
+# print('\n')
+# print("benzene: {}".format(benzene_list))
+# print('\n')
+#print("carboxylic acid group: {}".format(carboxylic_list))
+# print("silicon: {}".format(silicon_list))
+
+

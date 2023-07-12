3cb842dfe61c41cd59b23e2e07e0ba580fb34aa2
Vanessa Campbell <122318527+vanessa-b-campbell@users.noreply.github.com>
2023-07-12 09:10:39 -0400

    debugging functional group comparison with landon

--- get_functional_groups.py
+++ get_functional_groups.py
@@ -7,12 +7,12 @@ import re
 # using substructure searching
 
 # bring in the clean smiles- make the SMILES as a list
-SMILEs_data = pd.read_csv("/home/jbd3qn/Downloads/critical-Temp-LNN/clean_smile_dataset.csv")
+# SMILEs_data = pd.read_csv("/home/jbd3qn/Downloads/critical-Temp-LNN/clean_smile_dataset.csv")
+SMILEs_data = pd.read_csv("C:\\Users\\color\\Documents\\Bilodeau_Research_Python\\critical-Temp-LNN\\clean_smile_dataset.csv")
 
 
-# ('[Sn]', silicon_list),
 smiles_list = SMILEs_data['SMILEs'].tolist()
-# gutter_list = SMILEs_data['SMILEs'].tolist()
+
 
 # benzene, phenol group, carboxylic acid, amine
 phenol_list = []
@@ -28,21 +28,54 @@ argon_list = []
 bromine_list = []
 sulfur_list = []
 scandium_list = []
-func_group_list = [('C1=CC=C(C=C1)O', phenol_list, 'phenol'), ('C1=CC=CC=C1', benzene_list, 'benzene'), 
-                    ('C(=O)O', carboxylic_list, 'carboxylic acid'),# ('F', fluorine_list, 'fluorine')]#, 
-                   ('N', nitrogen_list, 'nirogen')] #, ("[Si]", silicon_list, 'silicon'), 
-                  # ('[Xe]', xe_list, 'Xe'), ('Cl', chlorine_list, 'chlorine'), 
-                  # ('[Ti]', thallium_list, 'thallium'), ('[Ar]', argon_list, 'Argon'),
-                  # ('Br', bromine_list, 'bromine'), ('S', sulfur_list, 'sulfur'), ('Sc', scandium_list, 'scandium')]
-
-gutter_list = []
-print(len(gutter_list))
+# make into list of lists
+func_group_list = [['C1=CC=C(C=C1)O', phenol_list, 'phenol'], ['C1=CC=CC=C1', benzene_list, 'benzene'], 
+                    ['C(=O)O', carboxylic_list, 'carboxylic acid']]# ('F', fluorine_list, 'fluorine')]#, 
+                #('N', nitrogen_list, 'nitrogen')] #, ("[Si]", silicon_list, 'silicon'), 
+                # ('[Xe]', xe_list, 'Xe'), ('Cl', chlorine_list, 'chlorine'), 
+                # ('[Ti]', thallium_list, 'thallium'), ('[Ar]', argon_list, 'Argon'),
+                # ('Br', bromine_list, 'bromine'), ('S', sulfur_list, 'sulfur'), ('Sc', scandium_list, 'scandium')]
 
 # Will need a list of pattern SMILE string
 # loop through each dataset smile for a given pattern
 # if true the name of the smile string, and maybe even the critical temp corresponding to it, 
 # needs to be added or put somewhere identifying it....
 # make a matrix? indexing will get ugly... debugging a nightmare
+new_smiles_with_brackets = []
+
+for smile in smiles_list: # for each smile
+    #print(smile)
+    new_smile = smile
+
+    if "Si" in new_smile or "Sc" in new_smile or "Cl" in new_smile: # super fast first-check that keeps going until no more groups are found
+
+        if new_smile.find('Si') != -1:      # smile.find returns -1 if the parameter 'Si' is not found, meaning "Si" exists if any number >= 0 is returned
+            index = new_smile.find('Si') # find index of this section of string
+            new_smile = new_smile[:index] + '[' + new_smile[index:index+2] + ']' + new_smile[index+2:] # create new smile to insert
+        if new_smile.find('Sc') != -1:
+            index = new_smile.find('Sc')
+            new_smile = new_smile[:index] + '[' + new_smile[index:index+2] + ']' + new_smile[index+2:]
+        if new_smile.find('Cl') != -1:
+            index = new_smile.find('Cl')
+            new_smile = new_smile[:index] + '[' + new_smile[index:index+2] + ']' + new_smile[index+2:]
+
+    print(new_smile)
+    new_smiles_with_brackets.append(new_smile)
+
+    
+
+
+
+
+        
+        # for char_index in range(0, len(smile)):
+        #     if (smile[char_index] == "S" and (smile[char_index+1]) == "i") or (smile[char_index] == "S" and \
+        #             (smile[char_index+1]) == "c") or (smile[char_index] == "C" and (smile[char_index+1]) == "l"):
+                
+        #         smile = smile[0:char_index] + '[' + smile[char_index:char_index+2] + ']' + smile[char_index+2:]
+        #         break
+        # print(smile)
+
 
 
 for func_i in range(0, len(func_group_list)):
@@ -52,22 +85,40 @@ for func_i in range(0, len(func_group_list)):
         #try: 
         match = mol.HasSubstructMatch(Chem.MolFromSmiles(func_group_list[func_i][0])) # will return a true or false
         if match: 
-            print('match')
+            #print('match')
             (func_group_list[func_i][1]).append(smile)
-        # elif smile not in gutter_list:
-        #     print('no match')
-        #     gutter_list.append(smile)
-        #     print(len(gutter_list))
 
-while check == False:
-    for smile in smiles_list:
-    #take smile and check if its in any of the list
-        for index in range(0,len(func_group_list)):
-       
-            if smile in func_group_list[func_i][1]:
-                check = True #stop the loop here
+    # Statistics
+    percent = (len(func_group_list[func_i][1])/len(smiles_list))*100
+    print('\n')
+    print("{:.2f}% of molecules have a {} group".format(percent,func_group_list[func_i][2]))
 
+
+gutter_list = []
+
+for smile in smiles_list:
+    used = False
+    for functional_group in func_group_list:
+        if smile in functional_group[1]:
+            used = True
+    
+    if not used:
         gutter_list.append(smile)
+
+#print(gutter_list)
+leftover = ( len(gutter_list) / len(smiles_list) ) * 100
+print("{:.2f}% of molecules are in no group".format(leftover))
+
+
+# check = False
+# while check:
+#     for smile in smiles_list:
+#     #take smile and check if its in any of the list
+#         for index in range(0,len(func_group_list)):
+    
+#             if smile in func_group_list[func_i][1]:
+#                 check = True #stop the loop here
+#                 gutter_list.append(smile)
             
 # the problem here is that a molecule might only be in one list, that lsit could be index 5. So if it loops
 # through the first list and doesn't find it, it will append it to gutter_list -DO NOT WANT
@@ -89,16 +140,19 @@ while check == False:
             # elif smile not in gutter_list:
             #     gutter_list.append(smile)
                 
-    # Statistics
-    percent = (len(func_group_list[func_i][1])/len(smiles_list))*100
-    print('\n')
-    print("{:.2f}% of molecules have a {} group".format(percent,func_group_list[func_i][2]))
-    
-leftover = (len(gutter_list)/len(smiles_list))*100
-print("{:.2f}% of molecules are in no group".format(leftover))
 
     
-print(sulfur_list)
+
+
+
+
+
+
+
+
+
+    
+# print(sulfur_list)
 
 # print("phenol: {}".format(phenol_list))
 # print('\n')

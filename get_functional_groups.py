25eeae15a89ad3e8a7e5d10591f6559e40c7b6e6
vanessa-b-campbell <colorfulwaves@gmail.com>
2023-07-12 17:48:24 -0400

    function group comparison fulling working now

--- get_functional_groups.py
+++ get_functional_groups.py
@@ -21,11 +21,10 @@ nitrogen_list = []
 silicon_list = []
 xe_list = []
 chlorine_list = []
-thallium_list = []
+titanium_list = []
 argon_list = []
 bromine_list = []
 sulfur_list = []
-scandium_list = []
 iodine_list = []
 toluene_list = []
 aniline_list = []
@@ -49,16 +48,16 @@ caesium_list = []
 germanium_list = []
 tellurium_list = []
 tin_list = []
+thallium_list = []
+hydrogen_list =[]
 
 func_group_list = [ ['C1=CC=CC=C1', benzene_list, 'benzene'], 
-                    ['C(=O)O', carboxylic_list, 'carboxylic acid'], ['[Ti]', thallium_list, 'thallium'], 
-                    
+                    ['C(=O)O', carboxylic_list, 'carboxylic acid'], ['[Ti]', thallium_list, 'thallium'],   
 
                 ['N', nitrogen_list, 'nitrogen'], ['P', phosphorus_list, 'phosphorus'],
                 
 
                 
-
                 ['S', sulfur_list, 'sulfur'], ['O', oxygen_list, 'oxygen' ],
                  
 
@@ -78,55 +77,17 @@ func_group_list = [ ['C1=CC=CC=C1', benzene_list, 'benzene'],
                 ['[Rn]', radon_list, 'radon'], ['[He]', helium_list, 'helium'],
                 ['[Ar]', argon_list, 'Argon'], ['[Xe]', xe_list, 'Xe'],
 
-                ['[Cs]', caesium_list, 'caesium']]
+                ['[Cs]', caesium_list, 'caesium'],['C', carbon_list, 'carbon'] ]
 
-
+# second list of metalloids and a few others 
 metalloid_group_list = [["[Si]", silicon_list, 'silicon'],['[Se]', selenium_list, 'selenium'], 
                         ['[As]', arsenic_list, 'arsenic'],['[Ge]', germanium_list, 'germanium'],
-                        ['[Sn]', tin_list, 'tin']]
-
-# wtf is wrong with the metalloids? 
-############# diobedient molecules/elements
-# ["[Si]", silicon_list, 'silicon'], 
-# ['[Se]', selenium_list, 'selenium'],
-
-# ['[As]', arsenic_list, 'arsenic'],
-# ['[Ge]', germanium_list, 'germanium'],
-# ['[Sn]', tin_list, 'tin'],
+                        ['[Sn]', tin_list, 'tin'], ['[Ti]',titanium_list, 'titanium'],['[H]',hydrogen_list, 'hydrogen']]
 
 
 # create a second loop for the trouble mols using SMARTS instead of SMILES
 
 nobel_gases = [neon_list, krypton_list, radon_list, helium_list, argon_list, xe_list ]
-
-# for smile in smiles_list: # for each smile
-#     #print(smile)
-#     new_smile = smile
-
-#     if "Si" in new_smile or "Sc" in new_smile or "Cl" in new_smile: # super fast first-check that keeps going until no more groups are found
-
-#         if new_smile.find('Si') != -1:      # smile.find returns -1 if the parameter 'Si' is not found, meaning "Si" exists if any number >= 0 is returned
-#             index = new_smile.find('Si') # find index of this section of string
-#             new_smile = new_smile[:index] + '[' + new_smile[index:index+2] + ']' + new_smile[index+2:] # create new smile to insert
-#         if new_smile.find('Sc') != -1:
-#             index = new_smile.find('Sc')
-#             new_smile = new_smile[:index] + '[' + new_smile[index:index+2] + ']' + new_smile[index+2:]
-#         if new_smile.find('Cl') != -1:
-#             index = new_smile.find('Cl')
-#             new_smile = new_smile[:index] + '[' + new_smile[index:index+2] + ']' + new_smile[index+2:]
-
-#     print(new_smile)
-#     new_smiles_with_brackets.append(new_smile)
-
-    
-        
-        # for char_index in range(0, len(smile)):
-        #     if (smile[char_index] == "S" and (smile[char_index+1]) == "i") or (smile[char_index] == "S" and \
-        #             (smile[char_index+1]) == "c") or (smile[char_index] == "C" and (smile[char_index+1]) == "l"):
-                
-        #         smile = smile[0:char_index] + '[' + smile[char_index:char_index+2] + ']' + smile[char_index+2:]
-        #         break
-        # print(smile)
 
 
 
@@ -140,8 +101,6 @@ for func_i in range(0, len(func_group_list)):
 
     # Statistics
     percent = (len(func_group_list[func_i][1])/len(smiles_list))*100
-    print('from functional group list #1:')
-    print('\n')
     print("{:.2f}% of molecules have {} group".format(percent,func_group_list[func_i][2]))
     if percent == 0.00:
         print('missed one: {}'.format(func_group_list[func_i][2]))
@@ -155,11 +114,10 @@ for func_i in range(0, len(metalloid_group_list)):
             (metalloid_group_list[func_i][1]).append(smile)
     # Statistic
     percent = (len(metalloid_group_list[func_i][1])/ len(smiles_list)) * 100
-    print('from metalloid list:')
-    print('\n')
     print("{:.2f}% of molecules have {} group".format(percent,metalloid_group_list[func_i][2]))
 
 
+func_group_list.extend(metalloid_group_list)
 
 gutter_list = []
 
@@ -172,31 +130,39 @@ for smile in smiles_list:
     if not used:
         gutter_list.append(smile)
 
-for smile in smiles_list:
-    used = False
-    for metalloid in metalloid_group_list:
-        if smile in metalloid_group_list[1]:
-            used = True
-    
-    if not used and smile not in gutter_list:
-        gutter_list.append(smile)
+for each in gutter_list:
+    print(each)
 
-#print(gutter_list)
+
+print('\n')
+
+
+# checking that all molecules are being defined and categorized
+# if gutter_list leftover is equal to 0.00% then all molecules are being added 
+# to at least one group type list
 leftover = ( len(gutter_list) / len(smiles_list) ) * 100
 print("{:.2f}% of molecules are in no group".format(leftover))
 
 
+
+# calculate the percentage of molecules that are aromatics
+# in theory there shouldn't be any duplicates in the aromatic type lists
+# this is based on the unlikely probability that one molecule will have more than
+# one aromatic type-- but this is a possible source of error in the calculated statistic
 aromatic_percent = (( len(phenol_list) + len(toluene_list) + len(aniline_list) + len(acetophenone_list) \
                     + len(benzaldehyde_list) + len(benzoic_acid_list) + len(benzonitrile_list) + len(ortho_xylene_list)\
                          + len(styrene_list) )/ len(smiles_list)  ) *100
 print("{:.2f}% of molecules have an aromatic group".format(aromatic_percent))
 
+
+
+
+# calculating the percentage of inorganic molecules
+# it's possible I missed one of the inorganic molecules in the back and forth
+# but unlikely
+# also assuming metalloids and noble gases are inorganic 
 inorganic_percent = ((len(caesium_list) + len(tin_list) + len(silicon_list) + len(germanium_list)\
                       + len(arsenic_list) + len(tellurium_list) + len(antimony_list) + len(helium_list)\
                         + len(neon_list)+ len(argon_list) + len(krypton_list) + len(xe_list)\
-                            + len(radon_list) + len(scandium_list))/ len(smiles_list)) * 100 
+                            + len(radon_list)  + len(titanium_list))/ len(smiles_list)) * 100 
 print("{:.2f}% of molecules are inorganic".format(inorganic_percent))
-
-
-for each in gutter_list:
-    print(each)

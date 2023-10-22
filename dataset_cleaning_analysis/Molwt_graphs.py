e54b1dbbdf7b78b45600f626c9223c0c340e02b5
Vanessa Campbell <122318527+vanessa-b-campbell@users.noreply.github.com>
2023-10-22 13:14:15 -0400

    color change on graphs

--- dataset_cleaning_analysis/Molwt_graphs.py
+++ dataset_cleaning_analysis/Molwt_graphs.py
@@ -21,11 +21,13 @@ import pandas as pd
 # 75%       181.129500
 # max       843.636000
 
-no_outliers = pd.read_csv('/home/jbd3qn/Downloads/critical-Temp-LNN/csv_data/No_outliers_smile_dataset.csv')
+# no_outliers = pd.read_csv('/home/jbd3qn/Downloads/critical-Temp-LNN/csv_data/No_outliers_smile_dataset.csv')
+no_outliers = pd.read_csv('C:\\Users\\color\\Documents\\Bilodeau_Research_Python\\Critical_Temp_Research\\critical-Temp-LNN\\csv_data\\No_outliers_smile_dataset.csv')
+
 cTemp_list = no_outliers['critical_temp']
 
 ############## Create size distribution plot using matplotlib
-plt.hist(molWt, bins = 25, edgecolor = 'black', color= '#EBA73B')
+plt.hist(molWt, bins = 25, edgecolor = 'black', color= '#A4B1FF')
 plt.xlabel('Mol weight (amu)',fontsize=18)
 plt.ylabel('frequency',fontsize=18)
 plt.title('Primarly Small Molecules Around 155 amu',fontsize=18)
@@ -33,7 +35,7 @@ plt.show()
 
 
 ############## Create critical temp distribution plot using matplotlib
-plt.hist(cTemp_list, bins = 100, edgecolor = 'black', color= '#EBA73B')
+plt.hist(cTemp_list, bins = 25, edgecolor = 'black', color= '#A4B1FF')
 plt.xlabel('critical temp (K)',fontsize=18)
 plt.ylabel('frequency',fontsize=18)
 plt.title('Critial Temperature Data',fontsize=18)
@@ -53,53 +55,53 @@ plt.show()
 
 
 ############## Create Tc vs molwt graph
-plt.scatter(molWt, cTemp_list, color= '#A9369E')
-plt.xlabel('Mol weight (amu)')
-plt.ylabel('Critical temperature (K)')
-plt.title('Critical Temperature (K) vs Molecular size (amu): cc = 0.215')
-plt.show()
+# plt.scatter(molWt, cTemp_list, color= '#A9369E')
+# plt.xlabel('Mol weight (amu)')
+# plt.ylabel('Critical temperature (K)')
+# plt.title('Critical Temperature (K) vs Molecular size (amu): cc = 0.215')
+# plt.show()
 
 
 
 
 
-############# extract valence electrons, create a distribution curve,
-#                   then a tc vs ve- plot
+# ############# extract valence electrons, create a distribution curve,
+# #                   then a tc vs ve- plot
 
-num_V_electrons = df_descriptors['NumValenceElectrons']
+# num_V_electrons = df_descriptors['NumValenceElectrons']
 
-# distribution
-plt.hist(num_V_electrons, bins = 50, edgecolor = 'black')
-plt.xlabel('number of valence electrons')
-plt.ylabel('frequency')
-plt.title('number of valence electrons')
-plt.show()
+# # distribution
+# plt.hist(num_V_electrons, bins = 50, edgecolor = 'black')
+# plt.xlabel('number of valence electrons')
+# plt.ylabel('frequency')
+# plt.title('number of valence electrons')
+# plt.show()
 
-# tc vs val electrons
-plt.scatter(num_V_electrons, cTemp_list)
-plt.xlabel('number of valence electrons')
-plt.ylabel('Critical temperature (K)')
-plt.title('Critical Temperature (K) vs number of valence electrons')
-plt.show()
-
-
-
-
-################### mol weight vs num of val electrons
-plt.scatter(molWt, num_V_electrons)
-plt.ylabel('number of valence electrons')
-plt.xlabel('Mol weight (amu)')
-plt.title('number of valence electrons vs Mol weight (amu) ')
-plt.show()
+# # tc vs val electrons
+# plt.scatter(num_V_electrons, cTemp_list)
+# plt.xlabel('number of valence electrons')
+# plt.ylabel('Critical temperature (K)')
+# plt.title('Critical Temperature (K) vs number of valence electrons')
+# plt.show()
 
 
 
 
-############## NumRadicalElectrons vs tc
-NumRadicalElectrons = df_descriptors['NumRadicalElectrons']
+# ################### mol weight vs num of val electrons
+# plt.scatter(molWt, num_V_electrons)
+# plt.ylabel('number of valence electrons')
+# plt.xlabel('Mol weight (amu)')
+# plt.title('number of valence electrons vs Mol weight (amu) ')
+# plt.show()
 
-plt.scatter(NumRadicalElectrons, cTemp_list)
-plt.xlabel('Num Radical Electrons')
-plt.ylabel('Critical temperature (K)')
-plt.title('Critical temperature (K) vs Num Radical Electrons: cc= 0.758')
-plt.show()
+
+
+
+# ############## NumRadicalElectrons vs tc
+# NumRadicalElectrons = df_descriptors['NumRadicalElectrons']
+
+# plt.scatter(NumRadicalElectrons, cTemp_list)
+# plt.xlabel('Num Radical Electrons')
+# plt.ylabel('Critical temperature (K)')
+# plt.title('Critical temperature (K) vs Num Radical Electrons: cc= 0.758')
+# plt.show()
--- dataset_cleaning_analysis/type_atoms_graphs.py
+++ dataset_cleaning_analysis/type_atoms_graphs.py
@@ -1,9 +1,11 @@
-from get_functional_groups import inorganic_percent, aromatic_percent
+# from get_functional_groups import inorganic_percent, aromatic_percent
 import matplotlib.pyplot as plt
 
+
+
 labels = ['inorganic', 'organic']
-sizes = [inorganic_percent, 100-inorganic_percent]
-colors = ['#D87608', '#EBA73B']
+sizes = [6.7, 100-6.7]
+colors = ['#46599E', '#A4B1FF']
 
 plt.pie(sizes, labels=labels, colors = colors, autopct='%1.1f%%', startangle=180, textprops={'fontsize': 14})
 plt.axis('equal')
@@ -22,11 +24,11 @@ plt.show()
 
 
 
-labels = ['Aromatic', 'non-aromatic organic molecules']
-sizes = [aromatic_percent, 100-aromatic_percent]
-colors = ['#D87608', '#EBA73B']
+# labels = ['Aromatic', 'non-aromatic organic molecules']
+# sizes = [aromatic_percent, 100-aromatic_percent]
+# colors = ['#D87608', '#EBA73B']
 
-plt.pie(sizes, labels=labels, colors = colors, autopct='%1.1f%%', startangle=180, textprops={'fontsize': 14})
-plt.axis('equal')
-plt.title('23.2% of organic SMILE molecules have an aromatic group',fontsize=18)
-plt.show()
+# plt.pie(sizes, labels=labels, colors = colors, autopct='%1.1f%%', startangle=180, textprops={'fontsize': 14})
+# plt.axis('equal')
+# plt.title('23.2% of organic SMILE molecules have an aromatic group',fontsize=18)
+# plt.show()
